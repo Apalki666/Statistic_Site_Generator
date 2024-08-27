58b66fe9aae8b9e1972ebfd394eb9dd15b62d2be
Apalki666 <catalinliviuborzan@gmail.com>
2024-08-27 19:47:33 +0300

    A

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from src.textnode import TextNode
+
+def main():
+    # Create a TextNode object
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    
+    # Print the object
+    print(node)
+
+# Don't forget to call main() at the end of the file
+main()
